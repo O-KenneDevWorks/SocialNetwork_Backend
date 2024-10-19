d422a89ac6d867d1fe51dd82572ba779983b7fda
OKenne <175995429+O-KenneDevWorks@users.noreply.github.com>
2024-10-19 14:16:24 -0400

    init commit

---
+++ src/models/thought.ts
@@ -0,0 +1,17 @@
+/* Components
+    thoughtText 
+        String
+        Required
+        Must be between 1 and 280 characters
+    createdAt 
+        Date
+        Set default value to the current timestamp
+        Use a getter method to format the timestamp on query
+    username string required
+        String
+        Required
+    reactions
+        Array of nested documents created with the reactionSchema
+
+    Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
+*/
